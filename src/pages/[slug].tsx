c008bb078ec26353ccfd35361575e4fc3ed7ae7a
Mark Woodward <mark@markwoodward.com.au>
2025-10-19 18:03:50 +1100

    links and styles

--- src/pages/[slug].tsx
+++ src/pages/[slug].tsx
@@ -1,15 +1,27 @@
 import { GetStaticPropsContext, GetStaticPaths } from 'next'
 import { allSlugs, postShow } from '@/lib/posts'
+import Link from 'next/link'
 
 export default function Post({ postData }: { postData: PostData }) {
   return (
-    <>
-    {postData.slug}
-    </>
+    <main className='max-w-lg mx-auto p-8'>
+      <h1 className='text-5xl font-serif'>{postData.title}</h1>
+      <div className='border border-white p-4 rounded'>
+        <article dangerouslySetInnerHTML={{ __html: postData.content }} />
+        <div className='flex justify-end text-zinc-500'>
+          { postData.date }
+        </div>
+      </div>
+      <div className='py-2'>
+        <Link href={`/`} className='text-lg'>
+          ← <span className='hover:underline text-sm'>Back to posts</span>
+        </Link>
+      </div>
+    </main>
   )
 }
 
-type PostData = ReturnType<typeof postShow>
+type PostData = Awaited<ReturnType<typeof postShow>>
 
 export async function getStaticProps(ctx: GetStaticPropsContext<{ slug: string }>) {
   const slug = ctx.params!.slug
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,25 @@
 import { GetStaticProps } from 'next'
+import Link from 'next/link'
 import { postsIndex } from '@/lib/posts'
 
 export default function Home({ allPosts }: { allPosts: AllPosts }) {
  return (
-  <>
-    <ul>
+  <main className='max-w-lg mx-auto p-8'>
+    <h1 className='text-5xl font-serif'>Posts</h1>
+    <ul className='border border-white p-4 rounded'>
        {allPosts.map(post => (
-         <li key="{post.slug}">{post.meta.title}</li>
+         <li key={ post.slug } className='border-b border-dotted p-2'>
+          <div className='w-full flex justify-between items-baseline'>
+            <h2 className='font-serif text-2xl'>
+              <Link href={`/${post.slug}`} title={post.meta.title} className='hover:underline'>{post.meta.title}</Link>
+            </h2>
+            { post.meta.date }
+          </div>
+          <div className='text-sm text-zinc-500'>{post.meta.excerpt}</div>
+        </li>
        ))}
     </ul>
-  </>
+  </main>
  )
 }
 
